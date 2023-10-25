ac8390fe9d7a5ef05e5b31dc09cb3af742f244ea
Jan Vackar <jackie4u@seznam.cz>
2023-10-25 16:50:45 +0200

    Created solution and Worker service project

---
+++ AltShiftTypo/AltShiftWorker/TextReader.cs
@@ -0,0 +1,15 @@
+﻿namespace AltShiftWorker;
+
+internal class TextReader {
+
+    public string GetSelectedText() {
+
+        //if (Clipboard.ContainsText(TextDataFormat.Text)) {
+        //    return Clipboard.GetText(TextDataFormat.Text);
+        //} else {
+        //return string.Empty;
+        //}
+        return "Honza";
+    }
+
+}
