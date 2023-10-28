9b55d77c7eb0a84b6fedc282da9fd886fef4a96a
Jan Vackar <jackie4u@seznam.cz>
2023-10-28 11:49:56 +0200

    First working PoC

--- AltShiftTypo/AltShiftApp/MainPage.xaml.cs
+++ AltShiftTypo/AltShiftApp/MainPage.xaml.cs
@@ -1,15 +1,32 @@
-﻿namespace AltShiftApp {
-    public partial class MainPage : ContentPage {
+﻿using System.Text;
 
-        public MainPage() {
-            InitializeComponent();
+namespace AltShiftApp;
+
+public partial class MainPage : ContentPage {
+
+    public MainPage() {
+        InitializeComponent();
+    }
+
+    private void OnTranslateBtnClicked(object sender, EventArgs e) {
+        
+        // Todo: <Medium> - Implement this for keystrokes instead of input string to handle edge cases with numbers etc.
+        var input = InputEditor.Text;
+
+        // Todo: <Medium> - Implement configurable keyboard maps
+        var keyMap = Data.KeyMapProvider.us2cz;
+
+        // Note: Remap each character with the map
+        var stringBuilder = new StringBuilder();
+        foreach (char key in input) {
+            if (keyMap.ContainsKey(key)) {
+                stringBuilder.Append(keyMap[key]);
+            } else {
+                stringBuilder.Append(key);
+            }
         }
 
-        private void OnTranslateBtnClicked(object sender, EventArgs e) {
+        OutputEditor.Text = stringBuilder.ToString();
 
-            var input = InputEditor.Text;
-            OutputEditor.Text = input;
-
-        }
     }
 }
